ea79d4eed71c1fa2079bd65ff21df5aa0a51b17c
nwdorian <neuwirthdorian@gmail.com>
2026-01-21 15:48:49 +0100

    add more movies to data seeding

--- MvcMovie/Data/SeedData.cs
+++ MvcMovie/Data/SeedData.cs
@@ -50,6 +50,166 @@ public static class SeedData
                     Genre = "Western",
                     Rating = "R",
                     Price = 3.99M,
+                },
+                new Movie
+                {
+                    Title = "The Godfather",
+                    ReleaseDate = DateTime.Parse("1972-3-24"),
+                    Genre = "Crime",
+                    Rating = "R",
+                    Price = 12.99M,
+                },
+                new Movie
+                {
+                    Title = "Pulp Fiction",
+                    ReleaseDate = DateTime.Parse("1994-10-14"),
+                    Genre = "Crime",
+                    Rating = "R",
+                    Price = 11.99M,
+                },
+                new Movie
+                {
+                    Title = "Forrest Gump",
+                    ReleaseDate = DateTime.Parse("1994-7-6"),
+                    Genre = "Drama",
+                    Rating = "PG-13",
+                    Price = 9.99M,
+                },
+                new Movie
+                {
+                    Title = "The Shawshank Redemption",
+                    ReleaseDate = DateTime.Parse("1994-9-23"),
+                    Genre = "Drama",
+                    Rating = "R",
+                    Price = 10.99M,
+                },
+                new Movie
+                {
+                    Title = "The Dark Knight",
+                    ReleaseDate = DateTime.Parse("2008-7-18"),
+                    Genre = "Action",
+                    Rating = "PG-13",
+                    Price = 14.99M,
+                },
+                new Movie
+                {
+                    Title = "Inception",
+                    ReleaseDate = DateTime.Parse("2010-7-16"),
+                    Genre = "Sci-Fi",
+                    Rating = "PG-13",
+                    Price = 13.99M,
+                },
+                new Movie
+                {
+                    Title = "Fight Club",
+                    ReleaseDate = DateTime.Parse("1999-10-15"),
+                    Genre = "Drama",
+                    Rating = "R",
+                    Price = 9.99M,
+                },
+                new Movie
+                {
+                    Title = "The Matrix",
+                    ReleaseDate = DateTime.Parse("1999-3-31"),
+                    Genre = "Sci-Fi",
+                    Rating = "R",
+                    Price = 11.99M,
+                },
+                new Movie
+                {
+                    Title = "Gladiator",
+                    ReleaseDate = DateTime.Parse("2000-5-5"),
+                    Genre = "Action",
+                    Rating = "R",
+                    Price = 12.99M,
+                },
+                new Movie
+                {
+                    Title = "Titanic",
+                    ReleaseDate = DateTime.Parse("1997-12-19"),
+                    Genre = "Romance",
+                    Rating = "PG-13",
+                    Price = 10.99M,
+                },
+                new Movie
+                {
+                    Title = "Jurassic Park",
+                    ReleaseDate = DateTime.Parse("1993-6-11"),
+                    Genre = "Adventure",
+                    Rating = "PG-13",
+                    Price = 9.99M,
+                },
+                new Movie
+                {
+                    Title = "Star Wars: Episode IV – A New Hope",
+                    ReleaseDate = DateTime.Parse("1977-5-25"),
+                    Genre = "Sci-Fi",
+                    Rating = "PG",
+                    Price = 8.99M,
+                },
+                new Movie
+                {
+                    Title = "Back to the Future",
+                    ReleaseDate = DateTime.Parse("1985-7-3"),
+                    Genre = "Adventure",
+                    Rating = "PG",
+                    Price = 7.99M,
+                },
+                new Movie
+                {
+                    Title = "Indiana Jones and the Raiders of the Lost Ark",
+                    ReleaseDate = DateTime.Parse("1981-6-12"),
+                    Genre = "Adventure",
+                    Rating = "PG",
+                    Price = 8.99M,
+                },
+                new Movie
+                {
+                    Title = "The Lion King",
+                    ReleaseDate = DateTime.Parse("1994-6-24"),
+                    Genre = "Animation",
+                    Rating = "G",
+                    Price = 6.99M,
+                },
+                new Movie
+                {
+                    Title = "Toy Story",
+                    ReleaseDate = DateTime.Parse("1995-11-22"),
+                    Genre = "Animation",
+                    Rating = "G",
+                    Price = 6.99M,
+                },
+                new Movie
+                {
+                    Title = "Saving Private Ryan",
+                    ReleaseDate = DateTime.Parse("1998-7-24"),
+                    Genre = "War",
+                    Rating = "R",
+                    Price = 11.99M,
+                },
+                new Movie
+                {
+                    Title = "Schindler's List",
+                    ReleaseDate = DateTime.Parse("1993-12-15"),
+                    Genre = "Historical Drama",
+                    Rating = "R",
+                    Price = 12.99M,
+                },
+                new Movie
+                {
+                    Title = "Avatar",
+                    ReleaseDate = DateTime.Parse("2009-12-18"),
+                    Genre = "Sci-Fi",
+                    Rating = "PG-13",
+                    Price = 13.99M,
+                },
+                new Movie
+                {
+                    Title = "The Avengers",
+                    ReleaseDate = DateTime.Parse("2012-5-4"),
+                    Genre = "Action",
+                    Rating = "PG-13",
+                    Price = 14.99M,
                 }
             );
             context.SaveChanges();
---
+++ MvcMovie/Migrations/20260121144510_AddRequiredColumns.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequiredColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
